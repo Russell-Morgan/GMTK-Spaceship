3c26e3b9b2f24ccf4bf5129bce44c1f3aa68ed4a
Javier Santillan <31017068+Reviani@users.noreply.github.com>
2022-07-17 00:45:01 -0500

    Volume Slider bug fix
    fixed bug where slider value doesnt stay between scene changes dispite volume staying the same

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
-    public static float VolumeAmount = 1f;
+    public static float VolumeAmount = 0.8f;
+    public Slider VolumeSlider;
    // private AudioSource[] hinge;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        if(VolumeSlider != null)
+        VolumeSlider.value = VolumeAmount;
+
         AudioListener.volume = VolumeAmount;
 
     }
